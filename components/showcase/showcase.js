e18373963a51b62c92d39a0906a54227fb212208
annBuho <annbuho@gmail.com>
2024-05-16 21:26:18 +0300

    add pizza card popup

--- components/showcase/showcase.js
+++ components/showcase/showcase.js
@@ -10,6 +10,8 @@ pizzaList.className = "showcase__list";
 
 data.forEach(pizza => {
 	const item = document.createElement('li');
+	const imgContainer = document.createElement('div');
+	const imgTile = document.createElement('div');
 	const img = document.createElement('img');
 	const title = document.createElement('h4');
 	const description = document.createElement('p');
@@ -17,6 +19,8 @@ data.forEach(pizza => {
 	const buttonInner = document.createElement('h5');
 
 	item.className = "pizza-card";
+	imgContainer.className = "pizza-card__img-container";
+	imgTile.className = "pizza-card__img-tile";
 	img.className = "pizza-card__img";
 	title.className = "pizza-card__title";
 	description.className = "pizza-card__description";
@@ -27,9 +31,19 @@ data.forEach(pizza => {
 	description.innerText = pizza.description;
 	buttonInner.innerText = PIZZA_CARD_BUTTON_TEXT;
 
+	imgTile.append(img);
+	imgContainer.append(imgTile);
 	button.append(buttonInner);
-	item.append(img, title, description, button);
+	item.append(imgContainer, title, description, button);
 	pizzaList.append(item);
+
+	imgContainer.addEventListener("click", () => {
+		if(imgContainer.classList.contains('full-size')) {
+			imgContainer.classList.remove('full-size');
+		} else {
+			imgContainer.classList.add('full-size');
+		}
+	});
 });
 
 showcase.append(pizzaList);
